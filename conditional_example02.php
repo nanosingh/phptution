9a2079b4095ec57fda54723ba606f48b50cb5432
nanosingh <161162984+nanosingh@users.noreply.github.com>
2024-03-15 09:01:05 +0530

    Add files via upload

---
+++ conditional_example02.php
@@ -0,0 +1,22 @@
+<?php
+
+$a =  100;
+$b = 200;
+$c = 30;
+
+if ($a > $b AND $a >$c)
+{
+    echo $a;
+}
+elseif ($b > $a AND $b > $c)
+{
+    echo $b;
+}
+else
+{
+    echo $c;
+}
+
+
+
+?>
---
+++ exp05.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Greeting Program</title>
+</head>
+<body>
+    <h2>Welcome to the Our Program</h2>
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+        Please Enter Divident: <input type="number" name="var1">
+        <br><br>
+        Please Enter Divisor: <input type="number" name="var2">
+        <br><br>
+        <input type="submit" name="submit" value="Submit">
+    </form>
+
+    <?php
+    /* Function to validate input
+    function test_input($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    
+    }
+    */
+    
+        $var1 = test_input($_POST["var1"]);
+        $var2 = test_input($_POST["var2"]);
+
+        echo '<br /><br/> <br />';
+
+
+        if ($var1 / $var2) {
+        // Display greeting and age after ten years
+        echo "Hello ! First Number is Greater than Second Number";
+    }
+    else 
+    {
+        echo "Hello ! Second Number is Greater than First Number";
+    }
+    ?>
+</body>
+</html>
