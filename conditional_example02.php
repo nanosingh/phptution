5312dd35cf7a2c1c08e3d9e9aa6c6fd7dfb6f184
nanosingh <161162984+nanosingh@users.noreply.github.com>
2024-03-16 07:24:20 +0530

    Add files via upload

--- conditional_example02.php
+++ conditional_example02.php
@@ -1,21 +1,64 @@
 <?php
 
-$a =  100;
-$b = 200;
-$c = 30;
+$a = 1;
+$b = 2;
+$c = 3;
 
-if ($a > $b AND $a >$c)
+if ($a < $b)
 {
-    echo $a;
+    if($b<$c)
+    {
+        echo $a;
+        echo "<br>";
+        echo $b;
+        echo "<br>";
+        echo $c;
+    }
+    elseif ($c<$b)
+    {
+        echo $a;
+        echo "<br>";
+        echo $c;
+        echo "<br>";
+        echo $b;
+    }
+    else
+    {
+        echo $a;
+        echo "<br>";
+        echo " Value of B = ". $b ."Value of C " .$c ;
+    }
 }
-elseif ($b > $a AND $b > $c)
+elseif ($b < $a) 
 {
-    echo $b;
+    if ($a<$c)
+    {
+        echo $b;
+        echo "<br>";
+        echo $a;
+        echo "<br>";
+        echo $c;
+    }
+    elseif ($c<$a) 
+    {
+        echo $b;
+        echo "<br>";
+        echo $c;
+        echo "<br>";
+        echo $a;
+    }
+    else
+    {
+        echo $b;
+        echo "<br>";
+        echo " Value of B =". $c ."Value of C " . $a; 
+    }
 }
-else
+else 
 {
-    echo $c;
+    echo "Equal Value = ". $a;
 }
+
 
 
 
