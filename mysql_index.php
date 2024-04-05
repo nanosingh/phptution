8f8c87a5360b98cbaf7f44c827954b34607448cb
nanosingh <161162984+nanosingh@users.noreply.github.com>
2024-04-05 12:53:26 +0530

    Update mysql_index.php

--- mysql_index.php
+++ mysql_index.php
@@ -1,1 +1,80 @@
-mys
+
+<pre>
+Database
+1. database create*
+2. select database
+3. database show
+4. Drop Database
+5. Copy Database
+
+Table & views
+- Create*
+- Alter*
+- show
+- Rename
+- Truncate
+- Describe
+- Drop
+- Copy
+- Repair
+- Add/Delete Column
+- Show Column
+- rename Column
+
+Query
+- MySQL Queries
+- Contrains
+- Insert Record*
+- Update Records
+- Delete Records
+- select Records*
+- replace
+- insert on duplicate key Update
+- insert ignore
+
+Clauses
+- where*
+- distinct
+- from*
+- order by*
+- group by
+- having
+
+Mysql Conditions
+- AND*
+- OR *
+- AND OR *
+- Bollean *
+- LIKE*
+- IN*
+- ANY
+- Exists
+- NOT*
+- not equal*
+- IS NULL*
+- IS NOT NULL*
+- BETWEEN*
+
+MySQL Join
+- Join
+- Inner Join
+- Left Join
+- Right Join
+- Cross Join
+- Self Join
+- Delete Join
+- Update Join
+- EquiJion
+- NAtural Join
+- Left Join vs Right Join
+- Union vs Join
+
+Functions
+- count()
+- sum()
+- avg()
+- min()
+- max()
+- group_concat()
+- first()
+- last()
